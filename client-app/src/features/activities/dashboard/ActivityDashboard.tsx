3182b1f5186ceb3e5bdb78a0f0c96da4db9213b4
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-07-14 21:45:32 -0400

    51 creating a details view

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetail from '../details/ActivityDetail';
 import ActivityList from './ActivityList';
 
 interface Props {
@@ -12,6 +13,10 @@ export default function ActivityDashboard({activities}: Props){
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && 
+                <ActivityDetail activity={activities[0]} /> }
             </Grid.Column>
         </Grid>
     )
