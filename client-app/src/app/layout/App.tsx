86c35241dcdb59fc6bd5065fb56c2915c687c9fb
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-07-18 17:46:48 -0400

    64 posing data to server for create and edit activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -13,6 +13,7 @@ function App() {
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     agent.Activities.list().then(response => {
@@ -44,10 +45,24 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity){
-    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
-                : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+
+    setSubmitting(true);
+    if(activity.id){
+        agent.Activities.update(activity).then(() => {
+          setActivities([...activities.filter(x => x.id !== activity.id), activity])
+          setSelectedActivity(activity);
+          setEditMode(false);
+          setSubmitting(false);
+        });
+    }else{
+        activity.id = uuid();
+        agent.Activities.create(activity).then(() => {
+          setActivities([...activities, activity]);
+          setSelectedActivity(activity);
+          setEditMode(false);
+          setSubmitting(false);
+        });
+    }
   }
 
   function handleDeleteActivity(id: string){
@@ -61,16 +76,17 @@ function App() {
       <NavBar openForm = {handleFormOpen} />
       <Container style={{marginTop:'7em'}}>
         <ActivityDashboard 
-        activities={activities} 
-        selectedActivity = {selectedActivity}
-        selectActivity = {handleSelectActivity}
-        cancelSelectActivity = {handleCancelSelectActivity}
-        editMode = {editMode}
-        openForm = {handleFormOpen}
-        closeForm = {handleFormClose}
-        createOrEdit = {handleCreateOrEditActivity}
-        deleteActivity = {handleDeleteActivity}
-        />
+          activities={activities} 
+          selectedActivity = {selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+          editMode = {editMode}
+          openForm = {handleFormOpen}
+          closeForm = {handleFormClose}
+          createOrEdit = {handleCreateOrEditActivity}
+          deleteActivity = {handleDeleteActivity}
+          submitting = {submitting}
+          />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,10 +14,11 @@ interface Props {
     openForm: (id: string) => void;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id:string) => void
+    deleteActivity: (id:string) => void;
+    submitting: boolean;
 } 
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit,deleteActivity}: Props){
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit,deleteActivity, submitting}: Props){
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -35,7 +36,11 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
                     openForm = {openForm}
                 /> }
                 {editMode && 
-                 <ActivityForm closeForm= {closeForm} activity={selectedActivity} createOrEdit = {createOrEdit}/>
+                 <ActivityForm 
+                    closeForm= {closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit = {createOrEdit} 
+                    submitting={submitting}/>
                 }
             </Grid.Column>
         </Grid>
