125033a95f5c07384fd25512ffc98a84698106c1
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-01 18:11:55 -0400

    144 creating a login form

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,4 @@
-using MediatR;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
@@ -9,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
 
