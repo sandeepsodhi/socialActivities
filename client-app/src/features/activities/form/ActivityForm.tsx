71d1847a54c76782947c8d7b93d0ac382fb49133
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-07-31 23:35:27 -0400

    120 cearinf resuable text area

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,7 @@ import { v4 as uuid } from 'uuid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
 
 export default observer(function ActivityForm() {
 
@@ -68,7 +69,7 @@ export default observer(function ActivityForm() {
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder='title' />
-                        <MyTextInput placeholder='Description' name='description' />
+                        <MyTextArea rows={3} placeholder='Description' name='description' />
                         <MyTextInput placeholder='Category' name='category' />
                         <MyTextInput placeholder='Date' name='date' />
                         <MyTextInput placeholder='City' name='city' />
