1f2942cdc8b9c8db1b994aab67f1ba0856a3aa1e
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-01 00:19:14 -0400

    123 date strategy

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -14,6 +14,7 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
+import { Activity } from '../../../app/models/activity';
 
 export default observer(function ActivityForm() {
 
@@ -22,12 +23,12 @@ export default observer(function ActivityForm() {
     const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore
     const { id } = useParams<{ id: string }>();
 
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         id: '',
         title: '',
         category: '',
         description: '',
-        date: '',
+        date: null,
         city: '',
         venue: ''
     });
