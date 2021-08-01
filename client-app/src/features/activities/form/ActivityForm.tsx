a04ef4797dfc38516065c72f4bd2a1b58e7329b9
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-07-31 23:11:25 -0400

    118 validation in formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,18 +3,19 @@ import React, { ChangeEvent } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-export default observer(function ActivityForm(){
+export default observer(function ActivityForm() {
 
     const history = useHistory();
-    const {activityStore} = useStore();
-    const  {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore 
-    const {id} = useParams<{id: string}>();
+    const { activityStore } = useStore();
+    const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore
+    const { id } = useParams<{ id: string }>();
 
     const [activity, setActivity] = useState({
         id: '',
@@ -26,8 +27,12 @@ export default observer(function ActivityForm(){
         venue: ''
     });
 
+    const validationSchema = Yup.object({
+        title: Yup.string().required('Activity title is required')
+    })
+
     useEffect(() => {
-        if(id) loadActivity(id).then(activity => setActivity(activity!));
+        if (id) loadActivity(id).then(activity => setActivity(activity!));
     }, [id, loadActivity])
 
     // function handleSubmit() {
@@ -49,21 +54,24 @@ export default observer(function ActivityForm(){
     //     setActivity({...activity, [name]: value});
     // }
 
-    if(loadingInitial) return <LoadingComponent content='Loading activity...' />
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
-                {({handleSubmit}) => (
+            <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+                {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <Field placeholder = 'Title'  name = 'title'/>
-                        <Field placeholder = 'Description' name = 'description' />
-                        <Field placeholder = 'Category'  name = 'category'/>
-                        <Field type="date" placeholder = 'Date' name = 'date'/>
-                        <Field placeholder = 'City' name = 'city'/>
-                        <Field placeholder = 'Venue' name = 'venue'/>
-                        <Button loading={loading} floated='right' positive type='submit' content = 'Submit'/>
-                        <Button as={Link} to ='/activities' floated = 'right' type='button' content = 'Cancel' /> 
+                        <FormField>
+                            <Field placeholder='Title' name='title' />
+                            <ErrorMessage name='title' render={error => <Label basic color='red' content = {error} />} />
+                        </FormField>
+                        <Field placeholder='Description' name='description' />
+                        <Field placeholder='Category' name='category' />
+                        <Field type="date" placeholder='Date' name='date' />
+                        <Field placeholder='City' name='city' />
+                        <Field placeholder='Venue' name='venue' />
+                        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+                        <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
                 )}
             </Formik>
