06c7e322415357bfc9cc6e589808c7a26b951ac1
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-07 15:41:13 -0400

    161 loading related data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -24,6 +24,11 @@ namespace Application.Activities
 
             public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
+                var activities = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
+
                 return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
             }
         }
