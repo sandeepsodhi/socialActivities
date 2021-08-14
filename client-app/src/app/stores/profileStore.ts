429f997644fc730cedc062e8eecc29986caac16a
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-14 11:21:46 -0400

    204 setting the main photo

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,12 +1,13 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
-import { Profile } from "../models/profile";
+import { Photo, Profile } from "../models/profile";
 import { store } from "./store";
 
 export default class ProfileStore {
     profile: Profile | null = null;
     loadingProfile = false;
     uploading = false;
+    loading = false;
 
     constructor() {
         makeAutoObservable(this);
@@ -39,9 +40,9 @@ export default class ProfileStore {
             const response = await agent.Profiles.uploadPhoto(file);
             const photo = response.data;
             runInAction(() => {
-                if(this.profile){
+                if (this.profile) {
                     this.profile.photos?.push(photo);
-                    if(photo.isMain && store.userStore.user){
+                    if (photo.isMain && store.userStore.user) {
                         store.userStore.setImage(photo.url);
                         this.profile.image = photo.url;
                     }
@@ -51,6 +52,25 @@ export default class ProfileStore {
         } catch (error) {
             console.log(error);
             runInAction(() => this.uploading = false);
+        }
+    }
+
+    setMainPhoto = async (photo: Photo) => {
+        this.loading = true;
+        try {
+            await agent.Profiles.setMainPhoto(photo.id);
+            store.userStore.setImage(photo.url);
+            runInAction(() => {
+                if (this.profile && this.profile.photos) {
+                    this.profile.photos.find(p => p.isMain)!.isMain = false;
+                    this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
+                    this.profile.image = photo.url;
+                    this.loading = false;
+                }
+            })
+        } catch (error) {
+            runInAction(() => this.loading = false);
+            console.log(error);
         }
     }
 }
