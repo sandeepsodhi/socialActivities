2a703135bbaf2142f74a782ed9e9f0b23ecf4038
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-15 17:01:16 -0400

    219 resolving the utc date

--- client-app/src/features/activities/details/ActivityDetail.tsx
+++ client-app/src/features/activities/details/ActivityDetail.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Grid} from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetailedChat from './ActivityDetailedChat';
@@ -10,16 +10,17 @@ import ActivityDetailedHeader from './ActivityDetailedHeader';
 import ActivityDetailedInfo from './ActivityDetailedInfo';
 import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 
-export default observer (function ActivityDetail() {
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore
-    const { id } = useParams<{id: string}>();
+export default observer(function ActivityDetail() {
+    const { activityStore } = useStore();
+    const { selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity } = activityStore
+    const { id } = useParams<{ id: string }>();
 
     useEffect(() => {
-        if(id) loadActivity(id);
-    }, [id, loadActivity]);
+        if (id) loadActivity(id);
+        return () => clearSelectedActivity();
+    }, [id, loadActivity, clearSelectedActivity]);
 
-    if(loadingInitial || !activity) return <LoadingComponent />
+    if (loadingInitial || !activity) return <LoadingComponent />
 
     return (
         <Grid>
@@ -29,7 +30,7 @@ export default observer (function ActivityDetail() {
                 <ActivityDetailedChat activityId={activity.id} />
             </Grid.Column>
             <Grid.Column width={6}>
-                <ActivityDetailedSidebar activity = {activity} />
+                <ActivityDetailedSidebar activity={activity} />
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import { Segment, Header, Comment, Loader } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store'
 import * as Yup from 'yup';
+import { formatDistanceToNow } from 'date-fns'
 
 interface Props {
     activityId: string;
@@ -35,6 +36,38 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                 <Header>Chat about this event</Header>
             </Segment>
             <Segment attached clearing>
+                <Formik
+                    onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
+                    initialValues={{ body: '' }}
+                    validationSchema={Yup.object({
+                        body: Yup.string().required()
+                    })}
+                >
+                    {({ isSubmitting, isValid, handleSubmit }) => (
+                        <Form className='ui form'>
+                            <Field name='body' >
+                                {(props: FieldProps) => (
+                                    <div style={{ position: 'relative' }}>
+                                        <Loader active={isSubmitting} />
+                                        <textarea
+                                            placeholder='Enter you comment (Enter to submit, SHIFT + enter for the new line)'
+                                            rows={2}
+                                            {...props.field}
+                                            onKeyPress={e => {
+                                                if (e.key === 'Enter' && e.shiftKey)
+                                                    return;
+                                                if (e.key === 'Enter' && !e.shiftKey) {
+                                                    e.preventDefault();
+                                                    isValid && handleSubmit();
+                                                }
+                                            }}
+                                        />
+                                    </div>
+                                )}
+                            </Field>
+                        </Form>
+                    )}
+                </Formik>
                 <Comment.Group>
                     {commentStore.comments.map(comment => (
                         <Comment key={comment.id}>
@@ -42,48 +75,14 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                             <Comment.Content>
                                 <Comment.Author as={Link} to={`/profiles/${comment.username}`} >{comment.displayName}</Comment.Author>
                                 <Comment.Metadata>
-                                    <div>{comment.createdAt}</div>
+                                    <div>{formatDistanceToNow(comment.createdAt)} ago</div>
                                 </Comment.Metadata>
                                 <Comment.Text style={{ whiteSpace: 'pre-wrap' }} >{comment.body}</Comment.Text>
                             </Comment.Content>
                         </Comment>
                     ))}
-                    <Formik
-                        onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
-                        initialValues={{ body: '' }}
-                        validationSchema={Yup.object({
-                            body: Yup.string().required()
-                        })}
-                    >
-                        {({ isSubmitting, isValid, handleSubmit }) => (
-                            <Form className='ui form'>
-                                <Field name='body' >
-                                    {(props: FieldProps) => (
-                                        <div style={{ position: 'relative' }}>
-                                            <Loader active={isSubmitting} />
-                                            <textarea
-                                                placeholder='Enter you comment (Enter to submit, SHIFT + enter for the new line)'
-                                                rows={2}
-                                                {...props.field}
-                                                onKeyPress={e => {
-                                                    if (e.key === 'Enter' && e.shiftKey)
-                                                        return;
-                                                    if (e.key === 'Enter' && !e.shiftKey) {
-                                                        e.preventDefault();
-                                                        isValid && handleSubmit();
-                                                    }
-                                                }}
-                                            />
-                                        </div>
-                                    )}
-                                </Field>
-                            </Form>
-                        )}
-                    </Formik>
-
                 </Comment.Group>
             </Segment>
         </>
-
     )
 })
