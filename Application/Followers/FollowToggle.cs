5224d8fdff3e6bf9f589493e1031a103be4dbaa1
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-15 21:08:47 -0400

    223 adding a handler for following

---
+++ Application/Followers/FollowToggle.cs
@@ -0,0 +1,62 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Followers
+{
+    public class FollowToggle
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string TargetUserName { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _conext;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext conext, IUserAccessor userAccessor)
+            {
+                _conext = conext;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var observer = await _conext.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                var target = await _conext.Users.FirstOrDefaultAsync(x => x.UserName == request.TargetUserName);
+
+                if (target == null) return null;
+
+                var following = await _conext.UserFollowings.FindAsync(observer.Id, target.Id);
+
+                if(following == null)
+                {
+                    following = new UserFollowing
+                    {
+                        Observer = observer,
+                        Target = target
+                    };
+                    _conext.UserFollowings.Add(following);
+                }
+                else
+                {
+                    _conext.UserFollowings.Remove(following);
+                }
+
+                var success = await _conext.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Failed to update following");
+            }
+        }
+    }
+}
