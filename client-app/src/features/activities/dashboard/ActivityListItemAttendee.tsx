107861dc1ca59eea1281ad5bb01524db7f33fa1f
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-15 23:05:17 -0400

    229 adding the UI for followers

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -10,6 +10,12 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({ attendees }: Props) {
+
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 2
+    }
+
     return (
         <List horizontal>
             {attendees.map(attendee => (
@@ -18,7 +24,10 @@ export default observer(function ActivityListItemAttendee({ attendees }: Props) 
                     key={attendee.username}
                     trigger={
                         <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-                            <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
+                            <Image
+                                style={attendee.following ? styles : null}
+                                bordered
+                                size='mini' circular src={attendee.image || '/assets/user.png'} />
                         </List.Item>
                     }>
                     <Popup.Content>
