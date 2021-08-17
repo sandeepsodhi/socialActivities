cc2c2f718534dd570888e239eb8f2b4a038d6491
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-16 23:13:33 -0400

    238 adding a pagination header

--- application/Core/PagingParams.cs
+++ application/Core/PagingParams.cs
@@ -3,8 +3,8 @@ namespace Application.Core
     public class PagingParams
     {
         private const int MaxPageSize = 50;
-        public int PageNumber { get; set; }
-        private int _pageSize = 2;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
         public int PageSize
         {
             get => _pageSize;
