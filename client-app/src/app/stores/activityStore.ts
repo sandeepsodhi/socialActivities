433a6fde2726f9ff951469785f8e3833a509d0c9
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-08-17 19:45:17 -0400

    240 adding paging parameters

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,7 +2,7 @@ import format from "date-fns/format";
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity, ActivityFormValues } from "../models/activity";
-import { Pagination } from "../models/pagination";
+import { Pagination, PagingParams } from "../models/pagination";
 import { Profile } from "../models/profile";
 import { store } from "./store";
 
@@ -14,9 +14,21 @@ export default class ActivityStore {
     loading = false;
     loadingInitial = false;
     pagination: Pagination | null = null;
+    pagingParams = new PagingParams();
 
     constructor() {
         makeAutoObservable(this)
+    }
+
+    setPagingParams = (pagingParams : PagingParams) => {
+        this.pagingParams = pagingParams;
+    }
+
+    get axiosParams() {
+        const params = new URLSearchParams();
+        params.append('pageNumber', this.pagingParams.pageNumber.toString());
+        params.append('pageSize', this.pagingParams.pageSize.toString());
+        return params;
     }
 
     get activitiesByDate() {
@@ -36,7 +48,7 @@ export default class ActivityStore {
     loadActivities = async () => {
         this.loadingInitial = true;
         try {
-            const result = await agent.Activities.list();
+            const result = await agent.Activities.list(this.axiosParams);
             result.data.forEach(activity => {
                 this.setActivity(activity);
             })
